e06747970e72dc160724cdfa895e59d8ac57f945
Milan Lenco <milan@zededa.com>
2023-05-24 09:41:39 +0200

    Simplify setup - remove CDI, kubevirt and longhorn
    Signed-off-by: Milan Lenco <milan@zededa.com>

--- pkg/k3s/cluster-init.sh
+++ pkg/k3s/cluster-init.sh
@@ -101,28 +101,28 @@ if [ ! -f /var/lib/all_components_initialized ]; then
 		touch /var/lib/k3s_initialized
 	fi
 
-	if [ ! -f /var/lib/kubevirt_initialized ]; then
-		# This patched version will be removed once the following PR https://github.com/kubevirt/kubevirt/pull/9668 is merged
-		logmsg "Installing patched Kubevirt"
-		kubectl apply -f /etc/kubevirt-operator.yaml
-		kubectl apply -f https://github.com/kubevirt/kubevirt/releases/download/${KUBEVIRT_VERSION}/kubevirt-cr.yaml
+	#if [ ! -f /var/lib/kubevirt_initialized ]; then
+	#	# This patched version will be removed once the following PR https://github.com/kubevirt/kubevirt/pull/9668 is merged
+	#	logmsg "Installing patched Kubevirt"
+	#	kubectl apply -f /etc/kubevirt-operator.yaml
+	#	kubectl apply -f https://github.com/kubevirt/kubevirt/releases/download/${KUBEVIRT_VERSION}/kubevirt-cr.yaml
 
-		#CDI (containerzed data importer) is need to convert qcow2/raw formats to Persistent Volumes and Data volumes
-		#Since CDI goes with kubevirt we install with that.
-		logmsg "Installing CDI version $CDI_VERSION" 
-		kubectl create -f https://github.com/kubevirt/containerized-data-importer/releases/download/$CDI_VERSION/cdi-operator.yaml
-		kubectl create -f https://github.com/kubevirt/containerized-data-importer/releases/download/$CDI_VERSION/cdi-cr.yaml
-		touch /var/lib/kubevirt_initialized
-	fi
+	#	#CDI (containerzed data importer) is need to convert qcow2/raw formats to Persistent Volumes and Data volumes
+	#	#Since CDI goes with kubevirt we install with that.
+	#	logmsg "Installing CDI version $CDI_VERSION"
+	#	kubectl create -f https://github.com/kubevirt/containerized-data-importer/releases/download/$CDI_VERSION/cdi-operator.yaml
+	#	kubectl create -f https://github.com/kubevirt/containerized-data-importer/releases/download/$CDI_VERSION/cdi-cr.yaml
+	#	touch /var/lib/kubevirt_initialized
+	#fi
 
-	if [ ! -f /var/lib/longhorn_initialized ]; then
-		# This patched version will be removed once the following PR https://github.com/longhorn/go-iscsi-helper/pull/63#pullrequestreview-1387207567 is merged
-		logmsg "Installing patched longhorn"
-		kubectl apply -f /etc/longhorn-config.yaml
-		touch /var/lib/longhorn_initialized
-	fi
+	#if [ ! -f /var/lib/longhorn_initialized ]; then
+	#	# This patched version will be removed once the following PR https://github.com/longhorn/go-iscsi-helper/pull/63#pullrequestreview-1387207567 is merged
+	#	logmsg "Installing patched longhorn"
+	#	kubectl apply -f /etc/longhorn-config.yaml
+	#	touch /var/lib/longhorn_initialized
+	#fi
 
-	if [ -f /var/lib/k3s_initialized -a -f /var/lib/kubevirt_initialized -a -f /var/lib/longhorn_initialized ]; then
+	if [ -f /var/lib/k3s_initialized ]; then # -a -f /var/lib/kubevirt_initialized -a -f /var/lib/longhorn_initialized ]; then
 		logmsg "All components initialized"
 		touch /var/lib/all_components_initialized
 	fi
